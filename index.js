58ff293c56f95a03dd4345a869d4d949c0b4eeda
Jbeezy18 <justinpoeh@gmail.com>
2023-10-30 16:08:07 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static("public"));
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/music", (req, res) => {
+    res.render("music.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/news", (req, res) => {
+    res.render("news.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
