ae2659cefebdb32ea81894916804898639c2f0ab
Jbeezy18 <justinpoeh@gmail.com>
2023-11-01 02:33:35 -0700

    rmv node-modules

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import { check, validationResult } from "express-validator";
+import nodemailer from "nodemailer";
 
 const app = express();
 const port = process.env.PORT || 3030;
 
+app.set('view-engine', 'ejs');
 
 app.use('/public', express.static("public"));
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
 app.get("/", (req, res) => {
@@ -24,6 +28,64 @@ app.get("/about", (req, res) => {
 app.get("/news", (req, res) => {
     res.render("news.ejs");
 });
+
+// app.get("/contact", (req, res) => {
+//     res.render("contact.ejs", { errors : '' });
+// });
+
+// app.post("/send", 
+//     [
+//         check('name').notEmpty().withMessage('Name is required'),
+//         check('email').isEmail().withMessage('Invalid email address'),
+//         check('subject').notEmpty().withMessage('Subject required'),
+//         check('message').notEmpty().withMessage('Message required')
+//     ], (req, res) => {
+
+//     const errors = validationResult(req);
+//     const nm_user = process.env.NM_USER;
+//     const nm_pass = process.env.NM_PASS;
+//     const cc_email = process.env.CC_EMAIL;
+
+//     if(!errors.isEmpty())
+//     {
+//         res.render('contact', { errors: errors.mapped() });
+//     }
+//     else
+//     {
+//         const transporter = nodemailer.createTransport({
+//             service : 'Yahoo',
+//             auth : {
+//                 user : 'CraigCahailBookings@yahoo.com',
+//                 pass : 'HouseMusic23',
+//             },
+//             host : 'smtp.mail.yahoo.com',
+//             port : 465,
+//             secure : true,
+//         });
+
+//         const mail_option = {
+//             from : 'CraigCahailBookings@yahoo.com',
+//             to : 'Justinpoeh@gmail.com',
+//             subject : 'Booking Inquiry',
+//             text : req.body.message
+//         };
+
+//         transporter.sendMail(mail_option, (error, info) => {
+//             if(error)
+//             {
+//                 console.log(error);
+//             }
+//             else
+//             {
+//                 res.redirect('/success');
+//             }
+//         });
+//     }
+// });
+
+// app.get('/sucess', (req, res) => {
+//     response.send('<h1>Your Message was Successfully Sent! We Will Get Back To You ASAP!</h1>');
+// });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
