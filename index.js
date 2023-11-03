6a7ce8ecfde3b503ccdc1243b65b9b6b321da8e8
Jbeezy18 <justinpoeh@gmail.com>
2023-11-03 11:35:39 -0700

    Cleaned and added booking form to index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
-import axios from "axios";
 import bodyParser from "body-parser";
 import { check, validationResult } from "express-validator";
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3030;
@@ -12,7 +14,7 @@ app.set('view-engine', 'ejs');
 app.use('/public', express.static("public"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true }));
-
+    
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
@@ -29,59 +31,86 @@ app.get("/news", (req, res) => {
     res.render("news.ejs");
 });
 
-// app.get("/contact", (req, res) => {
-//     res.render("contact.ejs", { errors : '' });
-// });
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs", { errors : '' });
+});
 
-// app.post("/send", 
-//     [
-//         check('name').notEmpty().withMessage('Name is required'),
-//         check('email').isEmail().withMessage('Invalid email address'),
-//         check('subject').notEmpty().withMessage('Subject required'),
-//         check('message').notEmpty().withMessage('Message required')
-//     ], (req, res) => {
+app.get("/success", (req, res) => {
+    res.render("success.ejs");
+});
 
-//     const errors = validationResult(req);
-//     const nm_user = process.env.NM_USER;
-//     const nm_pass = process.env.NM_PASS;
-//     const cc_email = process.env.CC_EMAIL;
+app.post("/send", 
+    [
+        check('name').notEmpty().withMessage('Name is required'),
+        check('email').isEmail().withMessage('Invalid email address'),
+        check('phone').isMobilePhone().isLength({ min: 10, max: 10 }).withMessage('Oops...Please try another phone number'),
+        check('message').notEmpty().withMessage('Message required')
+    ], (req, res) => {
 
-//     if(!errors.isEmpty())
-//     {
-//         res.render('contact', { errors: errors.mapped() });
-//     }
-//     else
-//     {
-//         const transporter = nodemailer.createTransport({
-//             service : 'Yahoo',
-//             auth : {
-//                 user : ,
-//             },
-//             host : '
-//         });
+    const errors = validationResult(req);
 
-//         const mail_option = {
-//             from :
-//             subject : 'Booking Inquiry',
-//             text : req.body.message
-//         };
+    if(!errors.isEmpty()) {
+        res.render('contact', { errors: errors.mapped() });
+    } else {
+        const mail_name = req.body.name;
+        const mail_email = req.body.email;
+        const mail_message = req.body.message;
 
-//         transporter.sendMail(mail_option, (error, info) => {
-//             if(error)
-//             {
-//                 console.log(error);
-//             }
-//             else
-//             {
-//                 res.redirect('/success');
-//             }
-//         });
-//     }
-// });
+        const emailMessage = (
 
-// app.get('/sucess', (req, res) => {
-//     response.send('<h1>Your Message was Successfully Sent! We Will Get Back To You ASAP!</h1>');
-// });
+            `NAME: ${mail_name},
+    
+            EMAIL: ${mail_email},
+    
+            MESSAGE: ${mail_message}`
+    
+    );
+        const nm_user = process.env.NM_USER;
+        const nm_pass = process.env.NM_PASS;
+        const cc_email = process.env.CC_EMAIL;
+        const nm_serv = process.env.NM_SERV;
+        const nm_host = process.env.NM_HOST;
+        const nm_ciphers = process.env.NM_CIPHERS;
+        const nm_sub = process.env.NM_SUB;
+
+        const transporter = nodemailer.createTransport({
+            service : nm_serv,
+            host : nm_host,
+            port : 587,
+            secure : false,
+            auth : {
+                user : nm_user,
+                pass : nm_pass
+            },
+            tls : {
+                    ciphers : nm_ciphers,
+            },
+        });
+
+        transporter.sendMail({
+            from : nm_user,
+            to : cc_email,
+            subject : nm_sub,
+            text : emailMessage,
+                    
+        },
+
+        (errors, info) => {
+
+            if(errors) {
+                console.log(errors);
+
+            } else {
+                res.redirect('/success');
+            }
+        });
+        
+    };    
+});    
+
+app.get('/success', (req, res) => {
+    res.render('<h1>Your Message was Successfully Sent! We Will Get Back To You ASAP!</h1>');
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
